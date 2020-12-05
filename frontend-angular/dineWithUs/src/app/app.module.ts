8772ac63186b069d8936a2f415060607631b0f15
Murari Mahith <murarimahith21@gmail.com>
2020-12-05 16:54:11 +0530

    adding content to header component and styling it

--- frontend-angular/dineWithUs/src/app/app.module.ts
+++ frontend-angular/dineWithUs/src/app/app.module.ts
@@ -5,6 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainHeaderComponent } from './shared/main-header/main-header.component';
 
+import { DxButtonModule } from 'devextreme-angular';
+import { DxToolbarModule } from 'devextreme-angular'; 
+import { from } from 'rxjs';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +16,9 @@ import { MainHeaderComponent } from './shared/main-header/main-header.component'
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DxButtonModule,
+    DxToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
