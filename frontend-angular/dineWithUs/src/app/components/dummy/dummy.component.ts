6f743fdb782ded45f911828ef488c59409c69e20
Murari Mahith <murarimahith21@gmail.com>
2020-12-17 01:11:10 +0530

    Mahith: added dummycomponent which is whole heart for editing and all other forms for admin

--- frontend-angular/dineWithUs/src/app/components/dummy/dummy.component.ts
+++ frontend-angular/dineWithUs/src/app/components/dummy/dummy.component.ts
@@ -2,12 +2,17 @@ import { Component, Injectable, OnInit } from '@angular/core';
 // import {AngularFireDatabase} from 'angularfire2/database'
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { AngularFireDatabase } from '@angular/fire/database';
-import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireStorage, AngularFireUploadTask } from '@angular/fire/storage';
 import { HttpClient } from '@angular/common/http';
 import {Inject} from '@angular/core';
 
 import { Videocard } from './../../models/NewVideoCard'
 import { FirebaseDatabaseService } from 'src/app/services/firebaseDatabaseService';
+import * as firebase from "firebase/app";
+import { AuthServiceFirebase } from './../../services/authServiceFirebase'
+import { VideoType } from 'src/app/models/VideoType';
+
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-dummy',
@@ -25,49 +30,160 @@ export class DummyComponent implements OnInit {
   topViewedVideoCards : Videocard[] = [];
   mostRecentVideoCards : Videocard[] = [];
 
-  constructor(@Inject(AngularFireDatabase) private db: AngularFireDatabase, @Inject(FirebaseDatabaseService) private service : FirebaseDatabaseService) { }
+  addCardSelectedBool : boolean = false;
+  deleteCardSelectedBool : boolean = false;
+  updateCardSelectedBool : boolean = false;
+
+  newCard : Videocard = {
+    cardID: 0,
+    description: "",
+    imageUrl: "",
+    title: "",
+    videoType: {comingsoon: false, featured: false, mostrecent: false, topviewed: false},
+    ytLink: ""
+  }
+
+  urlForView = "./../../../assets/images/nophoto.png";
+  basePath = "/images";
+  urlForVideoCard = ""
+  task : AngularFireUploadTask;
+
+  selectedRecordForEdit : boolean = false;
+  selectedCardForEditing;
+  selectedCardForEditingImageUrl :string = "";
+
+  constructor(@Inject(AngularFireDatabase) private db: AngularFireDatabase, @Inject(FirebaseDatabaseService) private service : FirebaseDatabaseService, @Inject(AngularFireStorage) private fireStorage : AngularFireStorage, @Inject(AuthServiceFirebase) private authService : AuthServiceFirebase) { }
+
+  videocardsdatastatic;
 
   ngOnInit(): void {
 
-    this.service.getAllVideoCards()
-    .subscribe(actions => {
-      this.testVar =actions.payload.val() as Videocard;
-      this.videoCards.push(this.testVar);  
-      //console.log(actions.payload.val().cardID)
+    //console.log(this.authService.getAuthState())
+    // console.log("above getauthstate")
+    // console.log(firebase.auth().currentUser)
+    var isUserLoggedIn = localStorage.getItem("loggedIn")
+    if(!(isUserLoggedIn == "true"))
+    {
+      window.location.href = "/admin";
+      console.log("working")
+    }
+    else
+    {
+      console.log("signed in as admin no issues")
+    }
 
-      //console.log(this.videoCards)
-
-      console.log("all videocards");
-      console.log(this.videoCards)
-
-      this.featuredVideoCards = this.videoCards.filter(o => o.videoType["featured"]==true);
-      console.log("featured videocards");
-      console.log(this.featuredVideoCards)
-
-      this.topViewedVideoCards = this.videoCards.filter(o => o.videoType["topviewed"]==true);
-      console.log("topviewed videocards");
-      console.log(this.topViewedVideoCards)
-
-      this.comingSoonVideoCards = this.videoCards.filter(o => o.videoType["comingsoon"]==true);
-      console.log("comingsoon videocards");
-      console.log(this.comingSoonVideoCards)
-
-      this.mostRecentVideoCards = this.videoCards.filter(o => o.videoType["mostrecent"]==true);
-      console.log("mostrecent videocards");
-      console.log(this.mostRecentVideoCards)
+    this.service.getVideoCardsList().snapshotChanges().pipe(
+      map(changes =>
+        changes.map(c =>
+          ({ key: c.payload.key, ...c.payload.val() })
+        )
+      )
+    ).subscribe(objectsFromDB => {
+        this.videocardsdatastatic = objectsFromDB
     });
+
     
-    const testobj = {
-      cardID: 2,
-      description: "How to make Batani chat in 3 minutes in Telugu",
-      imageUrl: "./../../../assets/images/logo.jpg",
-      title: "Batani Chat 2",
-      videoType: {comingsoon: true, featured: true, mostrecent: false, topviewed: false},
-      ytLink: "https://www.youtube.com/watch?v=TfNMoQrOeCU"
-    } as Videocard;
-    
-    //this below line works.
-    //this.service.createVideoCard(testobj);
-    
+  }
+
+  async selectedFile(event)
+  {
+    if(event.target.files)
+    {
+      var reader = new FileReader();
+      reader.readAsDataURL(event.target.files[0])
+      reader.onload = (event : any) => {
+        this.urlForView = event.target.result;
+      }
+    }
+    const file = event.target.files[0];
+    if(file)
+    {
+      const filePath = `${this.basePath}/${file.name}`;
+      this.task = this.fireStorage.upload(filePath, file);
+
+      (await this.task).ref.getDownloadURL().then(o => {this.urlForVideoCard = o;console.log(this.urlForVideoCard)})
+    }
+  }
+
+  async onSubmit()
+  {
+    console.log(this.newCard);
+    var vCardVariable = new Videocard();
+    var videoTypeVariable = new VideoType();
+    vCardVariable.title = this.newCard.title;
+    vCardVariable.ytLink = this.newCard.ytLink;
+    vCardVariable.description = this.newCard.description;
+    vCardVariable.imageUrl = this.urlForVideoCard;
+    videoTypeVariable.featured = this.newCard.videoType.featured;
+    videoTypeVariable.comingsoon = this.newCard.videoType.comingsoon;
+    videoTypeVariable.topviewed = this.newCard.videoType.topviewed;
+    videoTypeVariable.mostrecent = this.newCard.videoType.mostrecent;
+    vCardVariable.videoType = videoTypeVariable;
+    console.log(vCardVariable);
+    var videocardsdatastatic;
+    this.service.getVideoCardsList().snapshotChanges().pipe(
+      map(changes =>
+        changes.map(c =>
+          ({ key: c.payload.key, ...c.payload.val() })
+        )
+      )
+    ).subscribe(objectsFromDB => {
+        videocardsdatastatic = objectsFromDB        
+        vCardVariable.cardID = videocardsdatastatic[objectsFromDB.length-1].cardID+1;
+        console.log(vCardVariable.cardID);
+        console.log(vCardVariable);       
+    });
+    setTimeout(() =>{
+      this.service.createVideoCard(vCardVariable)
+      window.location.href="/";
+    },500); 
+  }
+
+  addCardSelected()
+  {
+    this.addCardSelectedBool = true;
+    this.deleteCardSelectedBool = false;
+    this.updateCardSelectedBool = false;
+  }
+  deleteCardSelected()
+  {
+    this.addCardSelectedBool = false;
+    this.deleteCardSelectedBool = true;
+    this.updateCardSelectedBool = false;
+  }
+  updateCardSelected()
+  {
+    this.addCardSelectedBool = false;
+    this.deleteCardSelectedBool = false;
+    this.updateCardSelectedBool = true;
+  }
+
+  logout()
+  {
+      this.authService.logOut()
+  }
+
+  deleteCard(id)
+  {
+    console.log("deleting",id);
+    console.log(this.videocardsdatastatic)
+    alert("you are deleting videocard: "+this.videocardsdatastatic.find(o => o.cardID == id).title +"there is no coming back")
+    this.service.deleteVideoCard(this.videocardsdatastatic.find(o => o.cardID == id).key).catch(o=>alert("something went wrong contact developer or try again"))
+  }
+
+  editCard(id)
+  {
+    this.selectedRecordForEdit = true;
+    this.selectedCardForEditing = this.videocardsdatastatic.find(o => o.cardID == id);
+    this.selectedCardForEditingImageUrl = this.selectedCardForEditing.imageUrl;
+    console.log(this.selectedCardForEditing)
+    console.log(this.selectedCardForEditing.imageUrl)
+    window.scrollTo(0,document.body.scrollHeight);
+  }
+  updateCard(id)
+  {
+    console.log(this.selectedCardForEditing);
+    this.service.updateVideoCard(this.videocardsdatastatic.find(o => o.cardID == id).key,this.selectedCardForEditing)
+    window.location.href = "/add";
   }
 }
